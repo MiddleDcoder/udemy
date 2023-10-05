c90a19f4795ae25e742d245599c372265834ef2d
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2023-10-06 07:53:27 +0800

    Language Attribute

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
-<html lang="ar">
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        :lang(ar) {
-            direction: rtl;
-        }
-    </style>
+
 </head>
 <body>
     <p>Hello World!</p>   
