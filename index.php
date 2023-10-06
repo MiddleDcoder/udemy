77607b3d254a576fd1603daf31124e3beb9bd950
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2023-10-06 19:46:53 +0800

    added wp_footer
    additional tag wp-head and wp_footer functions

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
+    <?php wp_head(); ?>
 </head>
 <body>
-    <p>Hello World!</p>   
+    <p>Hello World!</p>
+    <?php wp_footer(); ?> 
 </body>
 </html>
