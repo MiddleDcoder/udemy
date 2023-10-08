71f24ddfc8877801c575971bd6292b4df8933ecb
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2023-10-09 01:38:54 +0800

    Registering Styles
    using wp_register_style()

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,5 +1,16 @@
 <?php
 
 function u_enqueue() {
-    
+    wp_register_style(
+        'u_font_rubik_and_pacifico',
+        'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap'
+    );
+    wp_register_style(
+        'u_bootstrap_icons',
+        get_theme_file_uri('assets/bootstrap-icons/bootstrap-icons.css')
+    );
+    wp_register_style(
+        'u_theme',
+        get_theme_file_uri('assets/public/index.css')
+    );
 }
